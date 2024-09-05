acd5c4bbb20e8b5ffcb7f92143400c47712e896c
Armandooiou <L24550279@chihuahua2.tecnm.mx>
2024-09-05 16:28:08 -0600

    Add files via upload

---
+++ EVA1_8_OPERACIONES2/src/main/java/com/mycompany/eva1_8_operaciones2/EVA1_8_OPERACIONES2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_8_operaciones2;
+
+/**
+ *
+ * @author aaro1
+ */
+public class EVA1_8_OPERACIONES2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String nombre = "Armando";
+        String apellido = "Rodriguez";
+        String nombreCom;
+        //CADENA DE TEXTO LA OPERACION SE LLENA
+        //CONCATENACION -->UNIR CADENA DE TEXTO 
+        //El operador de concatenacion es el +
+        //cuando un simbolo tiene varias funciones
+        //se le llama sobrecarga (de operaciones)
+        nombreCom = nombre + "  " + apellido;
+        
+        System.out.println("El nombre es:  " + nombre );
+        System.out.println("El apellido es:  " + apellido );
+        System.out.println("El nombre es:  " + nombreCom );
+    
+    
+    
+    
+    
+    
+    }
+}
